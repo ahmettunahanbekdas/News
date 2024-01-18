6d8903b5f826b95e8994f7e72e3e01691d7ed4f3
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2024-01-18 15:22:49 +0300

    test git

---
+++ News/Controllers/HomeController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Ahmet Tunahan Bekdaş on 18.01.2024.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+    
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+    }
+   
+    //MARK: - Helpers
+    
+    private func configure() {
+        print("Selam")
+        print("test")
+    }
+}
+
